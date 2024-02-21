663491dd38d75b49342ff9bba5db836d57809c08
Alec Reynolds <alec+git@lando.dev>
2024-02-20 20:14:26 -0800

    Add in undocumented mssql option for backwards compatibility. (#93)
    * Add in undocumented mssql option for backwards compatibility.
    
    * Mssql dependency.

---
+++ builders/drupal-mssql.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const _ = require('lodash');
+const LandoMssql = require('./../node_modules/@lando/mssql/builders/mssql.js');
+
+// Builder
+module.exports = {
+  name: 'drupal-mssql',
+  parent: '_service',
+  builder: (parent, config) => class DrupalMssql extends LandoMssql.builder(parent, LandoMssql.config) {
+    constructor(id, options = {}) {
+      super(id, options, {services: _.set({}, options.name)});
+    };
+  },
+};
