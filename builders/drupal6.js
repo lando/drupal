73808c011ca63c37b614b3f2355a1ed5dcf74c4c
Aaron Feledy <aaron@arrowonesolutions.com>
2025-01-08 20:47:11 -0600

    fix drupal 6 drush version test

--- builders/drupal6.js
+++ builders/drupal6.js
@@ -14,8 +14,9 @@ module.exports = {
     confSrc: path.resolve(__dirname, '..', 'config', 'drupal6'),
     defaultFiles: {},
     php: '5.6',
-    // @NOTE: below seems to be the last known drush version that let you do
-    // drush si -y succesfully
+    // @NOTE: below seems to be the last known Drupal 6 compatible
+    // drush version that let you do drush si -y successfully.
+    // @see https://github.com/drush-ops/drush/issues/218#issuecomment-2580619527
     drush: '8.4.5',
   },
   builder: (parent, config) => class LandoDrupal6 extends parent {
